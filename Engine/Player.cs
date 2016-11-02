64b9e5d2fcbbc94ad33220356511b2edc651dad2
Bryce <bryce@trainerroad.com>
2016-11-01 22:44:27 -0700

    Adds player factory using XML & Form closing

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -14,12 +14,76 @@ namespace Engine
         public List<InventoryItem> Inventory { get; set; }
         public List<PlayerQuest> Quests { get; set; }
 
-        public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints) : base(currentHitPoints, maximumHitPoints)
+        private Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints) : base(currentHitPoints, maximumHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Inventory = new List<InventoryItem>();
             Quests = new List<PlayerQuest>();
+        }
+
+        public static Player CreateDefaultPlayer()
+        {
+            Player player = new Player(10, 10, 20, 0);
+            player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+            player.CurrentLocation = World.LocationByID(World.LOCATION_ID_HOME);
+
+            return player;
+        }
+
+        public static Player CreatePlayerFromXmlString(string xmlPlayerData)
+        {
+            try
+            {
+                XmlDocument playerData = new XmlDocument();
+
+                playerData.LoadXml(xmlPlayerData);
+
+                int currentHitPoints = XmlConvert.ToInt32(
+                    playerData.SelectSingleNode("/Player/Stats/CurrentHitPoints").InnerText);
+                int maximumHintPoints = XmlConvert.ToInt32(
+                    playerData.SelectSingleNode("/Player/Stats/MaximumHitPoints").InnerText);
+                int gold = XmlConvert.ToInt32(
+                    playerData.SelectSingleNode("/Player/Stats/Gold").InnerText);
+                int experiencePoints = XmlConvert.ToInt32(
+                    playerData.SelectSingleNode("/Player/Stats/ExperiencePoints").InnerText);
+
+                Player player = new Player(currentHitPoints, maximumHintPoints, gold, experiencePoints);
+
+                int currentLocationID = XmlConvert.ToInt32(
+                    playerData.SelectSingleNode("/Player/Stats/CurrentLocation").InnerText);
+                player.CurrentLocation = World.LocationByID(currentLocationID);
+
+                foreach (XmlNode node in playerData.SelectNodes("/Player/InventoryItems/InventoryItem"))
+                {
+                    int id = XmlConvert.ToInt32(node.Attributes["ID"].Value);
+                    int quantity = XmlConvert.ToInt32(node.Attributes["Quantity"].Value);
+
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        player.AddItemToInventory(World.ItemByID(id));
+                    }
+                }
+
+                foreach (XmlNode node in playerData.SelectNodes("/Player/PlayerQuests/PlayerQuest")) 
+                {
+                    int id = XmlConvert.ToInt32(node.Attributes["ID"].Value);
+                    bool isCompleted = XmlConvert.ToBoolean(node.Attributes["IsCopmleted"].Value);
+
+                    PlayerQuest playerQuest = new PlayerQuest(World.QuestByID(id));
+                    playerQuest.IsCompleted = isCompleted;
+
+                    player.Quests.Add(playerQuest);
+                }
+
+                return player;
+
+            }
+            catch
+            {
+                //If there was an error with the XML data, return a default player object
+                return Player.CreateDefaultPlayer();
+            }
         }
 
         public bool HasRequiredItemToEnterThisLocation(Location location)
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 using Engine;
 
@@ -17,18 +18,24 @@ namespace SuperAdventure
         private Player _player;
         private Monster _currentMonster;
 
+        private const string PLAYER_DATA_FILE_NAME = "PlayerData.xml";
+
         public SuperAdventure()
         {
             InitializeComponent();
 
-            _player = new Player(10, 10, 20, 0);
-            MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
-            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+            if(File.Exists(PLAYER_DATA_FILE_NAME))
+            {
+                _player = Player.CreatePlayerFromXmlString(File.ReadAllText(PLAYER_DATA_FILE_NAME));
+            }
 
-            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
-            lblGold.Text = _player.Gold.ToString();
-            lblExperience.Text = _player.Level.ToString();
-            lblLevel.Text = _player.Level.ToString();
+            else
+            {
+                _player = Player.CreateDefaultPlayer();
+            }
+
+            MoveTo(_player.CurrentLocation);
+            UpdatePlayerStats();
         }
 
         private void btnNorth_Click(object sender, EventArgs e)
@@ -500,6 +507,10 @@ namespace SuperAdventure
             rtbMessages.Text += "TEST" + Environment.NewLine;
         }
 
+        private void SuperAdventure_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(PLAYER_DATA_FILE_NAME, _player.ToXmlString());
+        }
     }
    
 }
