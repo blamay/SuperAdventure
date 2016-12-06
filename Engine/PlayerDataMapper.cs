84465705caf165723b7f50e42426fbc4ad5f36d0
Bryce <bryce@trainerroad.com>
2016-12-05 22:43:45 -0800

    Fixes SQL
    Adding MultipleActiveResultSets=true to the connection string fixed it
    but it's bad practice
    
    http://scottlilly.com/learn-c-by-building-a-simple-rpg-index/lesson-22-3-creating-the-sql-to-save-and-load-the-saved-game-data/

--- Engine/PlayerDataMapper.cs
+++ Engine/PlayerDataMapper.cs
@@ -6,63 +6,63 @@ using System.Threading.Tasks;
 
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Diagnostics;
 
 namespace Engine
 {
     public static class PlayerDataMapper
     {
-        private static readonly string _connectionString = "Data Source=(local);Initial Catalog=SuperAdventure;Integrated Security=True";
+        private static readonly string _connectionString = "Data Source=(local);Initial Catalog=SuperAdventure;Integrated Security=True;MultipleActiveResultSets=True";
 
         public static Player CreateFromDatabase()
         {
             try
             {
-                //This is our connection to the database
+                // This is our connection to the database
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    //Open the connection, so we can perform SQL commands
+                    // Open the connection, so we can perform SQL commands
                     connection.Open();
 
                     Player player;
 
-                    //Create a SQL command object, that uses the connection to
-                    //our database. The SqlCommand object is where we create our SQL Statement
+                    // Create a SQL command object, that uses the connection to our database
+                    // The SqlCommand object is where we create our SQL statement
                     using (SqlCommand savedGameCommand = connection.CreateCommand())
                     {
                         savedGameCommand.CommandType = CommandType.Text;
-
-                        //This SQL statement reads the first rows in the SavedGame table.
-                        //For this program, we should only ever have one row,
-                        //but this will ensure we only get one record in our SQL query results.
+                        // This SQL statement reads the first rows in teh SavedGame table.
+                        // For this program, we should only ever have one row,
+                        // but this will ensure we only get one record in our SQL query results.
                         savedGameCommand.CommandText = "SELECT TOP 1 * FROM SavedGame";
 
-                        //Use Execute Reader when you expect the query to return a row, or rows
+                        // Use ExecuteReader when you expect the query to return a row, or rows
                         SqlDataReader reader = savedGameCommand.ExecuteReader();
 
-                        //Check if the query did not return a row/record of data
-                        if(!reader.HasRows)
+                        // Check if the query did not return a row/record of data
+                        if (!reader.HasRows)
                         {
-                            //There is no data in the SavedGame table,
-                            //so return null (no saved player data)
+                            // There is no data in the SavedGame table, 
+                            // so return null (no saved player data)
                             return null;
                         }
 
-                        //Get the row/record from the data reader
+                        // Get the row/record from the data reader
                         reader.Read();
 
-                        // Get the column vlaues for the row/Record
+                        // Get the column values for the row/record
                         int currentHitPoints = (int)reader["CurrentHitPoints"];
                         int maximumHitPoints = (int)reader["MaximumHitPoints"];
                         int gold = (int)reader["Gold"];
                         int experiencePoints = (int)reader["ExperiencePoints"];
                         int currentLocationID = (int)reader["CurrentLocationID"];
 
-                        //Create the Player object, with the saved game values
-                        player = Player.CreatePlayerFromDatabase(currentHitPoints, maximumHitPoints, gold, experiencePoints, currentLocationID);
+                        // Create the Player object, with the saved game values
+                        player = Player.CreatePlayerFromDatabase(currentHitPoints, maximumHitPoints, gold,
+                            experiencePoints, currentLocationID);
                     }
 
-                    //Read the rows/record from the Quest table, and add them to the player
+                    // Read the rows/records from the Quest table, and add them to the player
                     using (SqlCommand questCommand = connection.CreateCommand())
                     {
                         questCommand.CommandType = CommandType.Text;
@@ -70,24 +70,24 @@ namespace Engine
 
                         SqlDataReader reader = questCommand.ExecuteReader();
 
-                        if(reader.HasRows)
+                        if (reader.HasRows)
                         {
-                            while(reader.Read())
+                            while (reader.Read())
                             {
                                 int questID = (int)reader["QuestID"];
                                 bool isCompleted = (bool)reader["IsCompleted"];
 
-                                //Build the PlayerQuest item, for this row
+                                // Build the PlayerQuest item, for this row
                                 PlayerQuest playerQuest = new PlayerQuest(World.QuestByID(questID));
                                 playerQuest.IsCompleted = isCompleted;
 
-                                //Add the PlayerQuest to the player's property
+                                // Add the PlayerQuest to the player's property
                                 player.Quests.Add(playerQuest);
                             }
                         }
                     }
 
-                    //Read the rows/records form the Inventory table, and add them to the player
+                    // Read the rows/records from the Inventory table, and add them to the player
                     using (SqlCommand inventoryCommand = connection.CreateCommand())
                     {
                         inventoryCommand.CommandType = CommandType.Text;
@@ -95,26 +95,37 @@ namespace Engine
 
                         SqlDataReader reader = inventoryCommand.ExecuteReader();
 
-                        if(reader.HasRows)
+                        if (reader.HasRows)
                         {
-                            while(reader.Read())
+                            while (reader.Read())
                             {
                                 int inventoryItemID = (int)reader["InventoryItemID"];
                                 int quantity = (int)reader["Quantity"];
 
-                                //Add the item to the player's inventory
+                                // Add the item to the player's inventory
                                 player.AddItemToInventory(World.ItemByID(inventoryItemID), quantity);
                             }
                         }
                     }
 
-                    //Now that the player has been built from the database, return it.
-                    return player; 
+                    // Now that the player has been built from the database, return it.
+                    return player;
                 }
             }
-            catch(Exception ex)
+            catch (Exception e)
             {
-                //Ignore errors. If there is an error, this function will return a "null" player.
+                // Ignore errors. If there is an error, this function will return a "null" player.
+                // at least one of these..
+                Console.WriteLine(e);
+               // MessageBox.Show(e);
+                Debug.WriteLine(e);
+
+                var inner = e.InnerException;
+                while (inner != null)
+                {
+                    //display / log / view
+                    inner = inner.InnerException;
+                }
             }
 
             return null;
@@ -126,10 +137,10 @@ namespace Engine
             {
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    //Open the connection, so we can perform SQL commands
+                    // Open the connection, so we can perform SQL commands
                     connection.Open();
 
-                    //Insert/Update data in SavedGame table
+                    // Insert/Update data in SavedGame table
                     using (SqlCommand existingRowCountCommand = connection.CreateCommand())
                     {
                         existingRowCountCommand.CommandType = CommandType.Text;
@@ -138,79 +149,77 @@ namespace Engine
                         // Use ExecuteScalar when your query will return one value
                         int existingRowCount = (int)existingRowCountCommand.ExecuteScalar();
 
-                        if(existingRowCount == 0)
+                        if (existingRowCount == 0)
                         {
-                            //There is no existing row, so do an INSERT
+                            // There is no existing row, so do an INSERT
                             using (SqlCommand insertSavedGame = connection.CreateCommand())
                             {
                                 insertSavedGame.CommandType = CommandType.Text;
                                 insertSavedGame.CommandText =
-                                    "INSERT INTO SavedGame " + "(CurrentHitPoints, MaximumHitPoints, Gold, ExperiencePoints, CurrentLocationID) "
-                                    + "VALUES " + "(@CurrentHitPoints, @MaximumHitPoints, @Gold, @ExperiencePoints, @CurrentLocationID)";
+                                    "INSERT INTO SavedGame " +
+                                    "(CurrentHitPoints, MaximumHitPoints, Gold, ExperiencePoints, CurrentLocationID) " +
+                                    "VALUES " +
+                                    "(@CurrentHitPoints, @MaximumHitPoints, @Gold, @ExperiencePoints, @CurrentLocationID)";
 
-                                //Pass the values from the player object, to the SQL query, using parameters
+                                // Pass the values from the player object, to the SQL query, using parameters
                                 insertSavedGame.Parameters.Add("@CurrentHitPoints", SqlDbType.Int);
                                 insertSavedGame.Parameters["@CurrentHitPoints"].Value = player.CurrentHitPoints;
-
                                 insertSavedGame.Parameters.Add("@MaximumHitPoints", SqlDbType.Int);
                                 insertSavedGame.Parameters["@MaximumHitPoints"].Value = player.MaximumHitPoints;
-
                                 insertSavedGame.Parameters.Add("@Gold", SqlDbType.Int);
                                 insertSavedGame.Parameters["@Gold"].Value = player.Gold;
-
                                 insertSavedGame.Parameters.Add("@ExperiencePoints", SqlDbType.Int);
                                 insertSavedGame.Parameters["@ExperiencePoints"].Value = player.ExperiencePoints;
-
                                 insertSavedGame.Parameters.Add("@CurrentLocationID", SqlDbType.Int);
                                 insertSavedGame.Parameters["@CurrentLocationID"].Value = player.CurrentLocation.ID;
 
-                                //Perform the SQL Command.
-                                //Use ExecuteNonQuery, because this query does not return any results.
+                                // Perform the SQL command.
+                                // Use ExecuteNonQuery, because this query does not return any results.
                                 insertSavedGame.ExecuteNonQuery();
                             }
                         }
-
                         else
                         {
-                            //There is an existing row, so do an UPDATE
+                            // There is an existing row, so do an UPDATE
                             using (SqlCommand updateSavedGame = connection.CreateCommand())
                             {
                                 updateSavedGame.CommandType = CommandType.Text;
                                 updateSavedGame.CommandText =
-                                    "INSERT INTO SavedGame " + "(CurrentHitPoints, MaximumHitPoints, Gold, ExperiencePoints, CurrentLocationID) "
-                                    + "VALUES " + "(@CurrentHitPoints, @MaximumHitPoints, @Gold, @ExperiencePoints, @CurrentLocationID)";
+                                    "UPDATE SavedGame " +
+                                    "SET CurrentHitPoints = @CurrentHitPoints, " +
+                                    "MaximumHitPoints = @MaximumHitPoints, " +
+                                    "Gold = @Gold, " +
+                                    "ExperiencePoints = @ExperiencePoints, " +
+                                    "CurrentLocationID = @CurrentLocationID";
 
-                                //Pass the values from the player object, to the SQL query, using parameters
-                                //Using parameters helps make your program more secure. It will prevent SQL injection attacks.
+                                // Pass the values from the player object, to the SQL query, using parameters
+                                // Using parameters helps make your program more secure.
+                                // It will prevent SQL injection attacks.
                                 updateSavedGame.Parameters.Add("@CurrentHitPoints", SqlDbType.Int);
                                 updateSavedGame.Parameters["@CurrentHitPoints"].Value = player.CurrentHitPoints;
-
                                 updateSavedGame.Parameters.Add("@MaximumHitPoints", SqlDbType.Int);
                                 updateSavedGame.Parameters["@MaximumHitPoints"].Value = player.MaximumHitPoints;
-
                                 updateSavedGame.Parameters.Add("@Gold", SqlDbType.Int);
                                 updateSavedGame.Parameters["@Gold"].Value = player.Gold;
-
                                 updateSavedGame.Parameters.Add("@ExperiencePoints", SqlDbType.Int);
                                 updateSavedGame.Parameters["@ExperiencePoints"].Value = player.ExperiencePoints;
-
                                 updateSavedGame.Parameters.Add("@CurrentLocationID", SqlDbType.Int);
                                 updateSavedGame.Parameters["@CurrentLocationID"].Value = player.CurrentLocation.ID;
 
-                                //Perform the SQL Command.
-                                //Use ExecuteNonQuery, because this query does not return any results.
+                                // Perform the SQL command.
+                                // Use ExecuteNonQuery, because this query does not return any results.
                                 updateSavedGame.ExecuteNonQuery();
                             }
                         }
                     }
 
-                    //The Quest and Inventory tables might have more, or less, rows in 
-                    //the database than what the player has in their properties.
-                    //So, when we save the player's game, we will delete all the old rows
-                    //and add in all new rows.
-                    //This is easier than trying to add/delete/update each individual rows
+                    // The Quest and Inventory tables might have more, or less, rows in the database
+                    // than what the player has in their properties.
+                    // So, when we save the player's game, we will delete all the old rows
+                    // and add in all new rows.
+                    // This is easier than trying to add/delete/update each individual rows
 
-                    //Delete existing Quest Rows
+                    // Delete existing Quest rows
                     using (SqlCommand deleteQuestsCommand = connection.CreateCommand())
                     {
                         deleteQuestsCommand.CommandType = CommandType.Text;
@@ -219,8 +228,8 @@ namespace Engine
                         deleteQuestsCommand.ExecuteNonQuery();
                     }
 
-                    //Insert Quest rows, from the player object
-                    foreach(PlayerQuest playerQuest in player.Quests)
+                    // Insert Quest rows, from the player object
+                    foreach (PlayerQuest playerQuest in player.Quests)
                     {
                         using (SqlCommand insertQuestCommand = connection.CreateCommand())
                         {
@@ -236,7 +245,7 @@ namespace Engine
                         }
                     }
 
-                    //Delete existing Inventory rows
+                    // Delete existing Inventory rows
                     using (SqlCommand deleteInventoryCommand = connection.CreateCommand())
                     {
                         deleteInventoryCommand.CommandType = CommandType.Text;
@@ -245,7 +254,7 @@ namespace Engine
                         deleteInventoryCommand.ExecuteNonQuery();
                     }
 
-                    //Insert Inventory rows, from the player object
+                    // Insert Inventory rows, from the player object
                     foreach (InventoryItem inventoryItem in player.Inventory)
                     {
                         using (SqlCommand insertInventoryCommand = connection.CreateCommand())
@@ -263,9 +272,19 @@ namespace Engine
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception e)
             {
-                //We are going to ignore errors, for now.
+                // We are going to ignore erros, for now.
+                Console.WriteLine(e);
+                // MessageBox.Show(e);
+                Debug.WriteLine(e);
+
+                var inner = e.InnerException;
+                while (inner != null)
+                {
+                    //display / log / view
+                    inner = inner.InnerException;
+                }
             }
         }
     }
